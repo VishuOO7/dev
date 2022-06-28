e5782d77709f85311c341727585f69f46507a6d7
VishuOO7 <gundelivishwanath@gmail.com>
2022-06-28 11:43:20 +0530

    project added to git

---
+++ even.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;  
+import java.lang.*;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		//Scanner sc=new Scanner(System.in); 
+		
+		for(i=0;i<=50;i++) {
+			if (i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
